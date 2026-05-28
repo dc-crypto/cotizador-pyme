bcb8767530cf8dddb39b2b697c542d0ef79b9691
Diego <dcastrol75@gmail.com>
2026-05-28 16:38:22 -0400

    fix database user_id

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+
 from supabase import create_client
 
 SUPABASE_URL = "https://kplcpanukoibdslteyfl.supabase.co"
@@ -5,12 +6,12 @@ SUPABASE_KEY = "sb_publishable_dBFl-nkZjqjMeCW305J1DQ_NrRFUqSP"
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-def obtener_tareas():
-    response = supabase.table("tareas").select("*").order("id").execute()
+def obtener_tareas(user_id):
+    response = supabase.table("tareas").select("*").eq("user_id", user_id).order("id").execute()
     return response.data
 
-def agregar_tarea(titulo):
-    supabase.table("tareas").insert({"titulo": titulo}).execute()
+def agregar_tarea(titulo, user_id):
+    supabase.table("tareas").insert({"titulo": titulo, "user_id": user_id}).execute()
 
 def completar_tarea(id):
     tarea = supabase.table("tareas").select("completada").eq("id", id).execute().data[0]
